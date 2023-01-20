48c5d230ff4450f0ca9ea0a41eb6a8b66bb5de74
0nlyyAlxn <70292271+0nlyyAlxn@users.noreply.github.com>
2023-01-19 20:03:11 -0600

    Update NotifySystemModule.lua

--- Modules/Network/NotifySystemCaller/NotifySystemModule.lua
+++ Modules/Network/NotifySystemCaller/NotifySystemModule.lua
@@ -1,43 +1,8 @@
 local NotifySystemModule = {}
-local GUI = nil
+local GUI = game:GetService("CoreGui"):FindFirstChild("STX_Nofitication")
 
-if game:GetService("RunService") then
-	
-	local STX_Nofitication = Instance.new("ScreenGui")
-	local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
-	
-	STX_Nofitication.Name = "STX_Nofitication"
-	STX_Nofitication.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-	
-	STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	STX_Nofitication.ResetOnSpawn = false
-
-	STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
-	STX_NofiticationUIListLayout.Parent = STX_Nofitication
-	STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-	STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-	
-	GUI = STX_Nofitication
-	
-else
-	
-	local STX_Nofitication = Instance.new("ScreenGui")
-	local STX_NofiticationUIListLayout = Instance.new("UIListLayout")
-
-	STX_Nofitication.Name = "STX_Nofitication"
-	STX_Nofitication.Parent = game:GetService("CoreGui")
-
-	STX_Nofitication.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-	STX_Nofitication.ResetOnSpawn = false
-
-	STX_NofiticationUIListLayout.Name = "STX_NofiticationUIListLayout"
-	STX_NofiticationUIListLayout.Parent = STX_Nofitication
-	STX_NofiticationUIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Right
-	STX_NofiticationUIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-	STX_NofiticationUIListLayout.VerticalAlignment = Enum.VerticalAlignment.Bottom
-
-	GUI = STX_Nofitication
+if not GUI then
+    GUI = local NotifySystemCaller = loadstring(game:HttpGet(('https://raw.githubusercontent.com/0nlyyAlxn/RobloxUtilities/main/Modules/Network/NotifySystemCaller/NotificationsHolder.lua')))()
 end
 
 function NotifySystemModule:Notify(nofdebug, middledebug, all)
